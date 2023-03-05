79988a1e10207a6cdd5a089235b1144dda74b3f0
Andrés Prieto <68924563+afprietoa@users.noreply.github.com>
2023-03-05 17:16:19 -0500

    updated HtmlValidator

---
+++ src/HtmlValidator.java
@@ -0,0 +1,37 @@
+import java.util.Queue;
+import java.util.Stack;
+
+
+public class HtmlValidator {
+
+	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+
+		Stack<HtmlTag> tags2 = new Stack<>();
+
+		while (!tags.isEmpty()){
+
+				if(tags.peek().isOpenTag()){
+					tags2.push(tags.remove());
+				}else{
+					if(tags.peek().isSelfClosing())
+						tags.remove();
+					else{
+						if(tags2.isEmpty())
+							return null;
+						if(tags.peek().matches(tags2.peek())){
+							tags2.pop();
+							tags.remove();
+						}
+						else
+							return tags2;
+					}
+				}
+		}
+
+
+		return tags2; // this line is here only so this code will compile if you don't modify it
+	}
+	
+
+}
+
